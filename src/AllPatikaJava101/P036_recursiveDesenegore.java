c1cb31f65c9e049126d47ddb41672b572000bddf
Samet Karakuş <abdulsametkarakus@outlook.com>
2022-11-17 22:52:16 +0300

    Java dilinde kullanıcıdan alınan n değerine göre aşağıdaki kurala uyan döngü kullanmadan bir "Recursive" metot yazıyoruz.

---
+++ src/AllPatikaJava101/P036_recursiveDesenegore.java
@@ -0,0 +1,28 @@
+package AllPatikaJava101;
+
+
+import java.util.Scanner;
+public class P036_recursiveDesenegore {
+
+    static boolean design(int a, int b) {
+        System.out.print(a + " ");
+        if (a <= 0) {
+            while (a != b) {
+                a += 5;
+                System.out.print(a + " ");
+            }
+            return true;
+        }
+        a -= 5;
+        return design(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen sayı giriniz:");
+        int num = scan.nextInt();
+        System.out.print("N Sayısı: " + num + "\n" + "Çıktısı: ");
+        design(num, num);
+    }
+
+}
